7343a4e29076345f7e93d3c7a9e5ccf130ca1c03
ElyesBradai <elyes.brad@gmail.com>
2026-03-29 00:36:18 -0400

    resolve conflicts

--- main/main.ino
+++ main/main.ino
@@ -2,23 +2,23 @@
 #include "VerticalDistanceSensor.h"
 #include "Motor.h"
 
-HorizontalDistanceSensor horizontalDistanceSensor(9, 10);
-VerticalDistanceSensor verticalDistanceSensor(11, 12);
-Motor motor(6);
+// HorizontalDistanceSensor horizontalDistanceSensor(9, 10);
+// VerticalDistanceSensor verticalDistanceSensor(11, 12);
+Motor motor(9);
 int counter = 0;
 
 void setup() {
   Serial.begin(9600);
   
-  horizontalDistanceSensor.begin();
-  verticalDistanceSensor.begin();
+  // horizontalDistanceSensor.begin();
+  // verticalDistanceSensor.begin();
   motor.begin();
 }
 
 void loop() {
   // horizontalDistanceSensor.printReading();
   // verticalDistanceSensor.printReading();
-
+  motor.updateFromDistance(40);
   // Serial.println("--------------------");
   // delay(500);
 }
