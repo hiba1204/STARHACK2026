c20e7b3a30e82f11900c2b186590f56b932addce
ElyesBradai <elyes.brad@gmail.com>
2026-03-29 00:30:44 -0400

    Keep remote version of main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,24 @@
-// #include "HorizontalDistanceSensor.h"
-// #include "VerticalDistanceSensor.h"
+#include "HorizontalDistanceSensor.h"
+#include "VerticalDistanceSensor.h"
+#include "Motor.h"
 
-// HorizontalDistanceSensor horizontalDistanceSensor(9, 10);
-// VerticalDistanceSensor verticalDistanceSensor(11, 12);
+HorizontalDistanceSensor horizontalDistanceSensor(9, 10);
+VerticalDistanceSensor verticalDistanceSensor(11, 12);
+Motor motor(6);
+int counter = 0;
 
-// void setup() {
-//   Serial.begin(9600);
+void setup() {
+  Serial.begin(9600);
+  
+  horizontalDistanceSensor.begin();
+  verticalDistanceSensor.begin();
+  motor.begin();
+}
 
-//   horizontalDistanceSensor.begin();
-//   verticalDistanceSensor.begin();
-// }
+void loop() {
+  // horizontalDistanceSensor.printReading();
+  // verticalDistanceSensor.printReading();
 
-// void loop() {
-//   horizontalDistanceSensor.printReading();
-//   verticalDistanceSensor.printReading();
-
-//   Serial.println("--------------------");
-//   delay(500);
-// }
+  // Serial.println("--------------------");
+  // delay(500);
+}
